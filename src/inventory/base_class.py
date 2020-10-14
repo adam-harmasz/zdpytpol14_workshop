d241e2f1ab05251229999f294bb7900da73afd75
Adam Harmasz <adam.harmasz@skygate.io>
2020-10-14 20:29:46 +0200

    Add base class with tests

---
+++ src/inventory/base_class.py
@@ -0,0 +1,17 @@
+import abc
+
+
+class BaseProduct(abc.ABC):
+    """Your code goes here! :D """
+
+    def __init__(self, price: float, quantitity: int, name: str) -> None:
+        self.name = name
+        self.price = price
+        self.quantity = quantitity
+
+    def __repr__(self):
+        return self.name
+
+
+class Product(BaseProduct):
+    pass
